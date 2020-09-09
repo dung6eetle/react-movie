7582b74a30771e195afdb6d313e040b8e5a67021
dung6eetle <tem09970967@gmail.com>
2020-09-09 13:11:36 +0300

    installated react modules

---
+++ src/redux/redux-store.js
@@ -0,0 +1,13 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { reducer as formReducer } from 'redux-form';
+import thunkMiddleware from "redux-thunk";
+
+let reducers = combineReducers({
+    
+    form: formReducer
+})
+
+let store = createStore(reducers, applyMiddleware(thunkMiddleware))
+window.store = store
+
+export default store
