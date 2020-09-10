f84333a0da9e156dcd4a9f7d2da53b628cbc1b3f
dung6eetle <tem09970967@gmail.com>
2020-09-10 11:55:54 +0300

    fixed ->  result block, popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -4,11 +4,12 @@ import classes from '../style/Result.module.css'
 function Popup({selected, closePopup}) {
     return (
         <div className={classes.popup}>
-            <div className={classes.popup_content}>
-                <div>{selected.original_title}<span>{selected.release_date}</span></div>
-                <div className={classes.popup_rating}>Rating:{selected.popularity}</div>
-            </div>
             <div className={classes.popup_close} onClick={closePopup}>X</div>
+            <div className={classes.popup_content}>
+                <div className={classes.popup_title}>{selected.original_title}<br></br><span> {selected.release_date}</span></div>
+                <div className={classes.popup_rating}>Rating: {selected.popularity}</div>
+                <div className={classes.popup_overview}> Discription: {selected.overview}</div>
+            </div>
         </div>
     )
 }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import classes from '../style/Result.module.css'
-
+import moviePhoto from '../img/film.jpg'
 function Result({id, result, openPopup}) {
     const imgUrl = 'https://image.tmdb.org/t/p/w500'
     return (
         <div className={classes.result} onClick={() => openPopup(id)}>
-            <img className={classes.result_img} src={ imgUrl + result.poster_path} />
+            <img className={classes.result_img} src={ result.poster_path != null ? imgUrl + result.poster_path : moviePhoto} />
             <div className={classes.result_title}>{result.title}</div>
         </div>
     )
 }
 
 export default Result
+// user.photos.small != null ? user.photos.small : userPhoto
